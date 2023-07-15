9399a4b869900840d12ecb7d5d9d8412b5ff6939
ilhomcekk <81365657+ilhomcekk@users.noreply.github.com>
2023-07-16 01:10:49 +0500

    updated

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ dbConnect();
 const server = http.createServer(app);
 
 // config
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
