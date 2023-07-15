cb2ccc996845b5428d6ea5dccc87004960b4ca2d
ilhomcekk <81365657+ilhomcekk@users.noreply.github.com>
2023-07-16 01:05:27 +0500

    updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
