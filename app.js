855bb641c3509fe0630857a9151f7373e2a22e13
ilhomcekk <81365657+ilhomcekk@users.noreply.github.com>
2023-07-16 11:40:17 +0500

    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const http = require("http");
 const dbConnect = require("./database");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 443;
 const routes = require("./routes/index");
 
 app.set("view engine", "ejs");
@@ -15,7 +15,7 @@ dbConnect();
 const server = http.createServer(app);
 
 // config
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
